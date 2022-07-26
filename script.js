51b2b6bd715ecaba3b416af77d7bcdf66f473a95
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-07-26 18:11:38 -0500

    added function to save to local storage

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var containerEl = $('.container');
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 
+
+
 // Display's Today's Date
 todayElement.text(todaysDate.format('dddd, MMMM Do'));
 
@@ -12,42 +14,59 @@ todayElement.text(todaysDate.format('dddd, MMMM Do'));
 function displayTimeBlocks() {
   for (var i = 0; i < hours.length; i++) {
 
-  // creates tags for time slots
-   var rowBlock = $('<div>');
-   var hourBlock = $('<div>');
-   var textInput = $('<textarea>');
-   var saveBtn = $('<button>');
-   var saveIcon = $('<i>');
-   
-   //adds classes to tags for time slot stylings
-   rowBlock.addClass('row time-block');
-   hourBlock.addClass('col-1 hour');
-   hourBlock.text(moment().set('hour', hours[i]).format('hA'));
-   textInput.addClass('col-10 description');
-   textInput.data('hour', hours[i]);
-   saveBtn.addClass('col-1 saveBtn');
-   saveIcon.addClass('fas fa-save');
-   
-   //appends tags to html
-   containerEl.append(rowBlock);
-   rowBlock.append(hourBlock);
-   rowBlock.append(textInput);
-   rowBlock.append(saveBtn);
-   saveBtn.append(saveIcon);
+    // creates tags for time slots
+    var rowBlock = $('<div>');
+    var hourBlock = $('<div>');
+    var textInput = $('<textarea>');
+    var saveBtn = $('<button>');
+    var saveIcon = $('<i>');
 
-   //colors the time slots based on current time
-   if (currentTime > hours[i]) {
-     textInput.addClass('past')
-   }
-   else if (currentTime < hours[i]) {
-     textInput.addClass('future')
-   }
-   else {
-     textInput.addClass('present')
-   };
-   
+
+    //adds classes to tags for time slot stylings
+    rowBlock.addClass('row time-block');
+    hourBlock.addClass('col-1 hour');
+    hourBlock.text(moment().set('hour', hours[i]).format('hA'));
+    textInput.addClass('col-10 description');
+    textInput.data('hour', hours[i]);
+    saveBtn.addClass('col-1 saveBtn');
+    saveIcon.addClass('fas fa-save');
+
+    //appends tags to html
+    containerEl.append(rowBlock);
+    rowBlock.append(hourBlock);
+    rowBlock.append(textInput);
+    rowBlock.append(saveBtn);
+    saveBtn.append(saveIcon);
+
+    //colors the time slots based on current time
+    if (currentTime > hours[i]) {
+      textInput.addClass('past')
+    }
+    else if (currentTime < hours[i]) {
+      textInput.addClass('future')
+    }
+    else {
+      textInput.addClass('present')
+    };
+
+    
+
   };
+  
+  //targets button element to call save function
+  containerEl.on('click', 'button', saveUserInput);
+  
+  //pulls value from element previous to <button> and saves to local storage
+    function saveUserInput(event) {
+      event.preventDefault();
+      var input = $(this.previousElementSibling).val();
+      var hourSlot = $(this.previousElementSibling).data('hour');
+     
+      localStorage.setItem(hourSlot, input);
+    }
+
 };
+
 
 
 displayTimeBlocks();
